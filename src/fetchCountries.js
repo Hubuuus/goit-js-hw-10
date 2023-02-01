ba2da62e1482bc783a808a61d63eec8ca7c77925
Hubert Dulas <111136742+Hubuuus@users.noreply.github.com>
2023-02-01 19:50:50 +0100

    end

---
+++ src/fetchCountries.js
@@ -0,0 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { countryListElement } from './index';
+import { countryInfoDetails } from './index';
+export const fetchCountries = name => {
+    console.log(name);
+    fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    )
+      .then(response => response.json())
+      // .then(json => console.log(json))
+      .then(json => {
+        console.log(json);
+        console.log(json.length);
+        if (json.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (json.length <= 10 && json.length > 1) {
+          Notify.info('The following matches were found.');
+          json.forEach(country => {
+            // Lists up to 10 countries that match entered value
+            countryListElement(country);
+          });
+        } else if (json.length === 1) {
+          Notify.info('The following match was found.');
+          json.forEach(country => {
+            // Creates html element for country with more detailed information
+            countryInfoDetails(country);
+          });
+        } else {
+          Notify.failure('Oops, there is no country with that name');
+        }
+      })
+    //   .catch(() => Notify.failure('Oops, there is no country with that name'));
+  };
+  
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import './css/styles.css';
+let _ = require('lodash');
+
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener('input',  _.debounce(() => {
+  fetchCountries(searchBox.value.trim());
+  countryList.innerHTML = " "
+  countryInfo.innerHTML = " "
+}, DEBOUNCE_DELAY)
+);
+
+export const countryListElement = country => {
+  countryList.insertAdjacentHTML(
+    'beforeend',
+    `
+  <li class="country-list__element">
+    <img src="${country.flags.svg}" alt="${country.flags.svg}"/>
+    <p>${country.name.common}</p>
+   </li>`
+  );
+};
+
+export const countryInfoDetails = country => {
+  countryInfo.innerHTML = `
+  <img src="${country.flags.svg}" alt="${country.flags.svg}"/>
+  <h2>${country.name.common}</h2>
+  <p><span class="bold">Capital:</span> ${country.capital}</p>
+  <p><span class="bold">Population:</span> ${country.population}</p>
+  <p><span class="bold">Languages:</span> ${Object.values(country.languages)}</>
+  `;
+};
+
+
